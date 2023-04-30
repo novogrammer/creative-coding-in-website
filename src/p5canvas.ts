3713b9d8d1d47b75667ee9559c234bffa693af99
Yusuke Kawamoto <novogrammer@gmail.com>
2023-05-01 00:24:27 +0900

    それっぽさを追加

--- src/p5canvas.ts
+++ src/p5canvas.ts
@@ -12,19 +12,23 @@ export function setupCanvas(canvasContainer:HTMLDivElement){
       const yQty=5*2;
       const size=p5.height/yQty;
       const xQty=Math.ceil(p5.width/size*0.5)*2;
+      p5.noStroke();
       p5.rectMode(p5.CENTER);
+      p5.colorMode(p5.HSB,100);
+      const colorA=p5.color(100*3/6,10,100);
+      const colorB=p5.color(100*5/6,100,100);
       for(let ix=xQty*-0.5;ix<=xQty*0.5;ix+=1){
         for(let iy=yQty*-0.5;iy<=yQty*0.5;iy+=1){
           const x=ix*size+p5.width*0.5;
           const y=iy*size+p5.height*0.5;
-          const randomValue=p5.noise(ix*0.5,iy*0.5,performance.now()*0.001);
-          p5.noStroke();
-          p5.fill(randomValue*255,randomValue*255,randomValue*255);
+          const randomValue=p5.noise(ix*0.1+xQty,iy*0.1+yQty,performance.now()*0.001*0.7);
+          const c=p5.lerpColor(colorA,colorB,randomValue);
+          p5.fill(c);
           p5.ellipse(x,y,size,size);
 
         }
-        p5.pop();
       }
+      p5.pop();
 
     }
     window.addEventListener("resize",()=>{
